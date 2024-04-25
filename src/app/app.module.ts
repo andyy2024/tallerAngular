d4ba6dcacdf95be6f4596a8f4812751f1cfd6f39
andyy2024 <a.boteror@uniandes.edu.co>
2024-04-24 20:58:20 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeriesModule } from './series/series.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SeriesModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/series/serie.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Serie } from './Serie'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SerieService {
+
+  private apiUrl: string = environment.baseUrl;
+
+
+  constructor(private http: HttpClient) { }
+
+  getSeries(): Observable<Serie[]> {
+    return this.http.get<Serie[]>(this.apiUrl);
+  }
+
+}
